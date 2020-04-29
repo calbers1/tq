cfbb762353970abf4c611f787ae7ca2b64414edf
calbers1 <calbers1@harding.edu>
2020-04-29 12:27:06 -0500

    Updates

---
+++ precache-manifest.3699aceb9adf11cbf838123668be0611.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccca6cb7244b24ba7094c6d64524fdb1",
+    "url": "/tq/index.html"
+  },
+  {
+    "revision": "bdb434cdaacc8422e526",
+    "url": "/tq/static/css/2.ff533707.chunk.css"
+  },
+  {
+    "revision": "9d9d35a2f51b8c6a4057",
+    "url": "/tq/static/css/main.3da7d685.chunk.css"
+  },
+  {
+    "revision": "bdb434cdaacc8422e526",
+    "url": "/tq/static/js/2.3a7c0ef9.chunk.js"
+  },
+  {
+    "revision": "85a3f180362828d3f4c87b1cc3b8415d",
+    "url": "/tq/static/js/2.3a7c0ef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d9d35a2f51b8c6a4057",
+    "url": "/tq/static/js/main.ba737546.chunk.js"
+  },
+  {
+    "revision": "245ee968ab868794617e",
+    "url": "/tq/static/js/runtime-main.19a3db29.js"
+  },
+  {
+    "revision": "0be119fc35fa16522ba125179b2cd898",
+    "url": "/tq/static/media/CongaSound-4.0be119fc.wav"
+  },
+  {
+    "revision": "15246aca180f76a54fd1ea6bfc3d7a6b",
+    "url": "/tq/static/media/JungleMenu.15246aca.wav"
+  },
+  {
+    "revision": "11261b0d5999019d10ec7b00a30ac014",
+    "url": "/tq/static/media/TeamLogo.11261b0d.wav"
+  },
+  {
+    "revision": "a7a0ba22a907fb287a8b27596207548d",
+    "url": "/tq/static/media/TeamLogo.a7a0ba22.svg"
+  },
+  {
+    "revision": "45731a0fb47adc947228336f19c0f0ed",
+    "url": "/tq/static/media/bgvideo.45731a0f.mp4"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/tq/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/tq/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/tq/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/tq/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/tq/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "e734977d3a50658cfc28508db9bbafb7",
+    "url": "/tq/static/media/fail-sound-effect.e734977d.wav"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/tq/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "4f990a2225b53497f3db4fee2e23718f",
+    "url": "/tq/static/media/game-success.4f990a22.wav"
+  },
+  {
+    "revision": "e07aa85e57741f2bb8434eca29c07633",
+    "url": "/tq/static/media/gameLogo.e07aa85e.svg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/tq/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/tq/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/tq/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/tq/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/tq/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/tq/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/tq/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/tq/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/tq/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/tq/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "d9b2660650a5da342c0f99ef249710f8",
+    "url": "/tq/static/media/stone-pick.d9b26606.wav"
+  },
+  {
+    "revision": "1e88488047d0c74a2aac72873bffa1fa",
+    "url": "/tq/static/media/stone.1e884880.wav"
+  }
+]);
